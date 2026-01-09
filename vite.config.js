721e77deac6f39cb4118826358ad5992d8f58f1c
차동진 <dstoryj@gmail.com>
2026-01-09 15:10:39 +0900

    Fix base path for vision-camp repository

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/C2G/', // GitHub Pages 저장소 이름에 맞게 수정
+  base: '/vision-camp/', // GitHub Pages 저장소 이름에 맞게 수정
 })
